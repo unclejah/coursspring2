e2eb836d8c17dd840169879904c3c3164592e80a
jah <celt8@yandex.ru>
2022-10-25 09:52:36 +0500

    - init commit

---
+++ src/main/java/ru/tt/coursspring2/Coursspring2Application.java
@@ -0,0 +1,13 @@
+package ru.tt.coursspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursspring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursspring2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tt/coursspring2/Coursspring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tt.coursspring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursspring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
